2a05b3b1ef0552b4225bf1631927615551b2ea25
bdafahim <badrudduza20@student.sust.edu>
2018-12-15 23:16:31 +0600

    House Owner Can Update his Existing Posts
    Owner Part Complete :+1:

--- EasyRent/app/src/main/java/com/example/bdafahim/easyrent/UpdateDelete.java
+++ EasyRent/app/src/main/java/com/example/bdafahim/easyrent/UpdateDelete.java
@@ -3,7 +3,9 @@ package com.example.bdafahim.easyrent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -16,6 +18,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class UpdateDelete extends AppCompatActivity {
 
@@ -73,13 +78,13 @@ public class UpdateDelete extends AppCompatActivity {
             }
         });
 
-        mRef.addValueEventListener(new ValueEventListener() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users")
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .child("Posts");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                // Toast.makeText(OwnerProfile.this,"Inside Datasnapshot1",Toast.LENGTH_SHORT).show();
-                PROCESS1(dataSnapshot);
+                collectData((Map<String,Object>)dataSnapshot.getValue());
             }
 
             @Override
@@ -88,10 +93,60 @@ public class UpdateDelete extends AppCompatActivity {
             }
         });
     }
-    private void PROCESS1(DataSnapshot dataSnapshot){
+    public void collectData(Map<String,Object> users)
+    {
+        ArrayList<String> array = new ArrayList<>();
+        for(Map.Entry<String,Object>entry:users.entrySet()){
+            Map singleUser = (Map) entry.getValue();
+
+           email.setText((String)singleUser.get("email"));
+           owner.setText((String)singleUser.get("owner_name"));
+           phone.setText((String)singleUser.get("phone_no"));
+           long temp = (Long)singleUser.get("fee");
+          // int ree = Integer.parseInt(temp);
+           fee.setText(Long.toString(temp));
+            area.setText((String)singleUser.get("area"));
+            road.setText((String)singleUser.get("road_no"));
+            type.setText((String)singleUser.get("type"));
+           double temp1 = (Double)singleUser.get("lati");
+           lati.setText(Double.toString(temp1));
+            temp1 = (Double)singleUser.get("longi");
+            longi.setText(Double.toString(temp1));
+            Log.d("UpdateDelete",Long.toString(temp));
+            Log.d("UpdateDelete",Double.toString(temp1));
+            Log.d("UpdateDelete",Double.toString(temp1));
+            Log.d("UpdateDelete",Double.toString(temp1));
+            Log.d("UpdateDelete",Double.toString(temp1));
+            house.setText((String)singleUser.get("house_no"));
+        }
+
 
     }
     private void PROCESS2(){
-
+        String house_no, road_no, address, phone_no, email_no,ownername,type1;
+        double lati1,longi1;
+        int rentfee;
+        house_no = house.getText().toString().trim();
+        road_no = road.getText().toString().trim();
+        address = area.getText().toString().trim();
+        phone_no=phone.getText().toString().trim();
+        email_no=email.getText().toString().trim();
+        ownername=owner.getText().toString().trim();
+        type1=type.getText().toString().trim();
+        rentfee=Integer.parseInt(fee.getText().toString());
+        lati1 = Double.parseDouble(lati.getText().toString());
+        longi1=Double.parseDouble(longi.getText().toString());
+        Rent_Add rent_add = new Rent_Add(house_no,ownername,road_no,address,phone_no,email_no,type1,rentfee,lati1,longi1,key);
+        mRef.setValue(rent_add).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(UpdateDelete.this,"Update Successfull",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                else
+                    Toast.makeText(UpdateDelete.this,task.getException().toString(),Toast.LENGTH_LONG).show();
+            }
+        })   ;
     }
 }
